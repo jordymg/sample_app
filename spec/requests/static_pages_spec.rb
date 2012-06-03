7089c3da84ef6b45cfb45efdf5416d48e913f13f
Martin Giordano <giojordy@gmail.com>
2012-06-03 19:13:52 -0300

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home Page" do
+
+    it "should have the h1 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Help Page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About Page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    it "should havr the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+end
