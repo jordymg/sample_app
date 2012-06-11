bbc6a43e882d0e2d84b34155c1eeedc091684bf7
Martin Giordano <giojordy@gmail.com>
2012-06-10 21:47:27 -0300

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home Page" do
 
     it "should have the h1 'Sample App'" do
@@ -11,7 +13,12 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -24,7 +31,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "#{base_title} | Help")
     end
   end
 
@@ -34,11 +41,23 @@ describe "Static Pages" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
-    it "should havr the right title" do
+    it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+                                :text => "#{base_title} | About Us")
     end
+  end
+
+  describe "Contact Page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+
   end
 
 end
